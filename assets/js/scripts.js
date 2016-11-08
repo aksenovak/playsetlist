81b67e2ac211ec1919835b0c316ce9597a51bb66
aksenovak <aksenovak@yandex.ru>
2016-11-09 01:27:02 +0300

    add material styles, fx render, add playback by setlist

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,121 @@
 $(function () {
     var bandNameInput = $('#bandNameInput'),
         getSetlistBtn = $('#getSetlistBtn'),
-        listsWrap = $('#listsWrap');
+        listsWrap = $('#listsWrap'),
+        removePlayer = $('#removePlayer'),
+        playlist = [],
+        playerWrap = $('#playerWrap');
+
+    $(document).ajaxStop(function () {
+        if(playlist.length > 0) {
+            renderPlayer(playlist);
+            loaderShow(false);
+        }
+    });
+
+    removePlayer.on('click', function (e) {
+        e.preventDefault();
+
+        $(playerWrap.find('#playerContainer')).remove();
+
+        playlist = [];
+
+        playerWrap.addClass('hidden').append('<div id="playerContainer"></div>');
+    });
+
+    bandNameInput.on('keydown', function(e) {
+        if (e.which === 13 || e.keyCode === 13) {
+            e.preventDefault();
+
+            var value = $.trim($(this).val());
+
+            if(value !== '') {
+                getSetlists(value);
+            }
+        }
+    });
 
     getSetlistBtn.on('click', function (e) {
         e.preventDefault();
 
-        $.when(getSetlistRequest(bandNameInput.val())).then(
+        var value = $.trim(bandNameInput.val());
+
+        if(value !== '') {
+            getSetlists(value);
+        }
+    });
+
+    function renderPlayer(list) {
+        playerWrap.removeClass('hidden');
+
+        var playerUP, playerReady;
+
+        onYouTubeIframeAPIReady();
+
+        function onYouTubeIframeAPIReady() {
+            playerUP = new YT.Player('playerContainer', {
+                events: {
+                    'onReady': onPlayerReady
+                }
+            });
+        }
+
+        function onPlayerReady() {
+            playerReady = true;
+
+            playerUP.cuePlaylist({
+                'playlist': list
+            });
+        }
+    }
+
+    function getSetlists(val) {
+        loaderShow(true);
+
+        playlist = [];
+
+        $.when(getSetlistRequest(val)).then(
             function (res) {
-                listsWrap.html(res);
+                loaderShow(false);
+                renderSetlist(res);
+            },
+            function (err) {
+                if(err.responseJSON.hasOwnProperty('error')) {
+                    loaderShow(false);
+
+                    if(err.status == 404) {
+                        renderAlert(err.responseJSON.error);
+                    }
+                }
+            }
+        );
+    }
+
+    function getPlaylist(songs) {
+        loaderShow(true);
+
+        $.when(getPlaylistRequest(songs)).then(
+            function (res) {
+                loaderShow(false);
+                debugger;
+            },
+            function (err) {
+                loaderShow(false);
+                debugger;
+            }
+        );
+    }
+
+    function getVideoId(song) {
+        $.when(getVideoIdRequest(song)).then(
+            function (res) {
+                playlist.push(res.videoId);
             },
             function (err) {
                 debugger;
             }
-        )
-    });
+        );
+    }
 
     function getSetlistRequest(name) {
         return $.ajax({
@@ -24,5 +125,101 @@ $(function () {
                 artistName: name
             }
         });
+    }
+
+    function getPlaylistRequest(songs) {
+        return $.ajax({
+            url: '/get-playlist/',
+            method: 'GET',
+            data: {
+                songs: songs
+            }
+        });
+    }
+
+    function getVideoIdRequest(song) {
+        return $.ajax({
+            url: '/get-videoId/',
+            method: 'GET',
+            data: {
+                song: song
+            }
+        });
+    }
+
+    function loaderShow(status) {
+        var mainLoader = $('#mainLoader');
+
+        if(status) {
+            mainLoader.removeClass('hidden');
+        } else {
+            setTimeout(function () {
+                mainLoader.addClass('hidden');
+            }, 350);
+        }
+    }
+
+    function renderSetlist(template) {
+        listsWrap.html(template);
+        listsWrap.on('click', 'a.set-list-btn', function (e) {
+            e.preventDefault();
+            generatePlaylist($(this));
+        });
+    }
+
+    function generatePlaylist(element) {
+        var songs_list_wrap = $(element.data('setlist-wrap')),
+            song_list_ol = $(songs_list_wrap.find('ol')),
+            songs_array = [];
+
+        song_list_ol.find('li').each(function () {
+            songs_array.push($(this).data('song'));
+        });
+
+        if(songs_array.length > 0) {
+            loaderShow(true);
+
+            $.each(songs_array, function (i, song) {
+                getVideoId(song);
+            });
+        }
+    }
+
+    function renderAlert(text) {
+        var alertsWrap = $('#alertsWrap'),
+            current_uid  = guid(),
+            template = '<div id="'+current_uid+'" class="alert alert-warning">' +
+                '<button type="button" class="close"><span aria-hidden="true">&times;</span></button>' +
+                '<strong>Warning!&nbsp;</strong><span>'+text+'</span>' +
+                '</div>';
+
+        alertsWrap.append(template);
+
+        var current_alert = alertsWrap.find('#'+current_uid),
+            current_alert_btn = current_alert.find('button');
+
+        $(current_alert_btn).on('click', function (e) {
+            e.preventDefault();
+
+            $(this).parent().remove();
+        });
+
+        setTimeout(function () {
+            $(current_alert).animate({
+                opacity: 0
+            }, "slow", function () {
+                $(current_alert).remove();
+            });
+        }, 5000);
+    }
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
     }
 });
--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var express = require('express'),
     path = require('path'),
     request = require('request'),
-    exp_hbs  = require('express-handlebars');
-
-var app = express();
-
-var hbs = exp_hbs.create({
-    defaultLayout: 'main',
-    helpers: {
-        checkEmpty: function (value, key) {
-            return !!value.hasOwnProperty(key);
-        }
-    }
-});
+    YouTube = require('youtube-node'),
+    exp_hbs  = require('express-handlebars'),
+    app = express(),
+    hbs = exp_hbs.create({
+        defaultLayout: 'main'
+    }),
+    yt_key = 'AIzaSyBJAxhVEZPvj8pkzBPWXwerut3GwRfz3ZY';
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -31,7 +26,7 @@ app.get('/', function (req, res) {
 app.get('/get-setlist', function (req, res) {
     if(req.query.hasOwnProperty('artistName')) {
         request('http://api.setlist.fm/rest/0.1/search/setlists.json/?artistName='+req.query.artistName, function (error, response, body) {
-            if(!error && response.statusCode == 200) {
+            if(!error && response.statusCode === 200) {
                 var response_data = JSON.parse(body);
 
                 if(response_data.hasOwnProperty('setlists') && response_data.setlists.setlist.length > 0) {
@@ -41,8 +36,63 @@ app.get('/get-setlist', function (req, res) {
                         setlists:setlists,
                         layout:false});
                 }
+            } else {
+                if(response.statusCode === 404) {
+                    res.status(404).json({error: body});
+                }
             }
         });
+    }
+});
+
+app.get('/get-videoId', function (req, res) {
+    if(req.query.hasOwnProperty('song')) {
+        var youtube = new YouTube(),
+            song = req.query.song;
+
+        youtube.setKey(yt_key);
+
+        youtube.search(song, 2, function(error, result) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                res.send({videoId:result.items[0].id.videoId});
+            }
+        });
+    }
+});
+
+app.get('/get-playlist',  function (req, res) {
+    if(req.query.hasOwnProperty('songs')) {
+        var songs = req.query.songs;
+
+        if(songs.length > 0) {
+            var youTube = new YouTube(),
+                playlist = [],
+                i;
+
+            youTube.setKey(yt_key);
+
+            for(i = 0; i < songs.length; i++) {
+                youTube.search(songs[i], 2, function(error, result) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        playlist.push(result.items[0].id.videoId);
+                    }
+                });
+            }
+
+            if(i === songs.length) {
+                console.info(playlist);
+            }
+
+            if(playlist.length > 0) {
+                res.send(playlist);
+            }
+        }
     }
 });
 
